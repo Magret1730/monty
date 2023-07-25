473810f87e232b059d2de498a35875995ae46f5c
Magret <oyedeleabiodun79@gmail.com>
2023-07-25 13:53:30 -0700

    implemented the add function

--- main.c
+++ main.c
@@ -48,6 +48,8 @@ int read_and_process_file(FILE *file)
 			pop(&stack, line_number);
 		else if (strcmp(opcode, "swap") == 0)
 			swap(&stack, line_number);
+		else if (strcmp(opcode, "add") == 0)
+			add(&stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- pop_swap.c
+++ pop_swap.c
@@ -38,3 +38,24 @@ void swap(stack_t **stack, unsigned int line_number)
 	temp->next = (*stack)->next;
 	(*stack)->next = temp;
 }
+
+/**
+ * add - adds the top two elements of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: argv[1]
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
