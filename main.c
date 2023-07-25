a98e726af499d1fdaac94ab0be43794af7fc9369
Magret <oyedeleabiodun79@gmail.com>
2023-07-25 13:18:21 -0700

    implemented swap function

--- main.c
+++ main.c
@@ -46,6 +46,8 @@ int read_and_process_file(FILE *file)
 			pint(&stack, line_number);
 		else if (strcmp(opcode, "pop") == 0)
 			pop(&stack, line_number);
+		else if (strcmp(opcode, "swap") == 0)
+			swap(&stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
