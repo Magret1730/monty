3a70e1abed3a06bed83d8b97334c00a096810fca
Magret <oyedeleabiodun79@gmail.com>
2023-07-25 14:14:03 -0700

    implemented sub function

--- main.c
+++ main.c
@@ -50,6 +50,8 @@ int read_and_process_file(FILE *file)
 			swap(&stack, line_number);
 		else if (strcmp(opcode, "add") == 0)
 			add(&stack, line_number);
+		else if (strcmp(opcode, "sub") == 0)
+			sub(&stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
