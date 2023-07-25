1dc7e8972b9b1aa25cae0c8c1efe1489c010d130
Magret <oyedeleabiodun79@gmail.com>
2023-07-25 12:16:11 -0700

    implemented pop function

--- monty.h
+++ monty.h
@@ -41,5 +41,6 @@ void push(stack_t **stack, int value_to_push);
 void pall(stack_t **stack);
 void free_stack(stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif
