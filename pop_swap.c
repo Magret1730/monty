fa84dff1b57a4760a301911b2ffc31000f5dad29
Magret <oyedeleabiodun79@gmail.com>
2023-07-26 05:33:18 -0700

    implemented div function

--- pop_swap.c
+++ pop_swap.c
@@ -79,14 +79,14 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
 
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        temp = *stack;
-        (*stack)->next->n -= (*stack)->n;
-        *stack = (*stack)->next;
-        (*stack)->prev = NULL;
-        free(temp);
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	(*stack)->next->n -= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
 }
