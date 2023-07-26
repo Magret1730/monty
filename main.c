11ab0db7dd1cf8923e0be88d7c17ba267bd54101
Magret <oyedeleabiodun79@gmail.com>
2023-07-26 13:31:39 -0700

    implemented rotr function

--- main.c
+++ main.c
@@ -66,6 +66,8 @@ int read_and_process_file(FILE *file)
 			pstr(&stack, line_number);
 		else if (strcmp(opcode, "rotl") == 0)
 			rotl(&stack, line_number);
+		else if (strcmp(opcode, "rotr") == 0)
+			rotr(&stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- rotl.c
+++ rotl.c
@@ -21,3 +21,31 @@ void rotl(stack_t **stack, unsigned int line_number)
 	first->next = NULL;
 	*stack = second;
 }
+
+
+/**
+ * rotr -  rotates the stack to the bottom
+ * @stack: Pointer to the top of the stack
+ * @line_number: argv[1]
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current, *prev;
+
+	(void)line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	current = *stack;
+	prev = NULL;
+	while (current->next != NULL)
+	{
+		prev = current;
+		current = current->next;
+	}
+
+	prev->next = NULL;
+	current->next = *stack;
+	*stack = current;
+}
