9f6c011856029c24abc628f665ecb694d0a8ab52
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-07-26 07:03:12 -0700

    mul function prototype added

--- monty.h
+++ monty.h
@@ -48,5 +48,6 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void division(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
 
 #endif
